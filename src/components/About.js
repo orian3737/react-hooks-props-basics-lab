cb6081915afb9d834d533c09e766e0fe1b8971f9
Orian shillz <91493524+orian3737@users.noreply.github.com>
2024-06-09 23:22:38 -0400

    cool

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ image, bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {bio && <p>{bio}</p>}
+      <img src={image} alt="I made this" />
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
 
 export default About;
+
